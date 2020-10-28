59262877f283e5e28347dc99501c239228685931
Shardul <71704201+Shardul04@users.noreply.github.com>
2020-10-28 17:08:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project-27-done
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var bobObject1, bobObject2, bobObject3, bobObject4, bobObject5;
+var roof;
+var rope1,rope2,rope3,rope4,rope5;
+
+
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	bobObject1 = new Bob(250,300, 40);
+	bobObject2 = new Bob(291,300, 40);
+	bobObject3 = new Bob(332,300, 40);
+    bobObject4 = new Bob(373,300, 40);
+	bobObject5 = new Bob(414,300, 40);
+
+	roof = new Roof(382,100);
+
+	rope1=new Rope(bobObject1.body,roof.bottomBody,-80,0);
+  rope2=new Rope(bobObject2.body,roof.bottomBody,-40,0);
+  rope3=new Rope(bobObject3.body,roof.bottomBody,0,0);
+  rope4=new Rope(bobObject4.body,roof.bottomBody,40,0);
+  rope5=new Rope(bobObject5.body,roof.bottomBody,80,0);
+
+  
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(200);
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  roof.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+  if (keyCode===UP_ARROW){
+    Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45})
+  }
+}
